60f58fca81fb3a7336ba1d601573c3b7641de612
Damien Azzopardi <damien@datadidakt.com>
2024-11-17 17:30:20 +0000

    Fix error

--- models/staging/stg_listings.sql
+++ models/staging/stg_listings.sql
@@ -5,7 +5,7 @@
 select
     cast(id as string) as id,
     cast(host_id as integer) as host_id,
-    --cast(neighbourhood_cleansed as string) as neighbourhood_cleansed,
+    cast(neighbourhood_cleansed as string) as neighbourhood_cleansed,
     cast(room_type as string) as room_type,
     cast(price as numeric) as price,
     cast(minimum_nights as integer) as minimum_nights,
