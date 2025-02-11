b9cf24dc9c0143a5d5d93e46da21b8292743e4e2
Damien Azzopardi <damien@datadidakt.com>
2025-02-11 17:19:04 +0000

    adding-spotify-datasets-and-models

---
+++ models/staging/stg_spotify/stg_spotify__songs.sql
@@ -0,0 +1,17 @@
+with
+
+cleaned_songs as (
+
+    select
+        song_id,
+        upper(title) as title,
+        upper(artist) as artist,
+        album,
+        release_year,
+        coalesce(genre, 'Unknown') as genre
+    
+    from {{ source('spotify', 'songs') }}
+
+)
+
+select * from cleaned_songs
