c158db15078cf449b641600ac1b69a776c86297d
Damien Azzopardi <damien@datadidakt.com>
2025-02-18 11:27:42 +0000

    Fix mrt_listings_neighbourhood.sql

--- models/marts/mrt_airbnb/mrt_listings_neighbourhood.sql
+++ models/marts/mrt_airbnb/mrt_listings_neighbourhood.sql
@@ -4,10 +4,20 @@
     ) 
 }}
 
-select
-    neighbourhood_cleansed,
-    room_type,
-    count(id) as total_listings,
-    avg(price) as avg_price,
-from {{ ref('stg_airbnb__listings') }}
-group by 1,2
+with neighbourhood_prices as (
+
+    select
+        neighbourhood_cleansed,
+        room_type,
+        count(id) as total_listings,
+        round(avg(price),2) as avg_price
+
+    from {{ ref('stg_airbnb__listings') }}
+
+    group by 
+        neighbourhood_cleansed,
+        room_type
+
+)
+
+select * from neighbourhood_prices
