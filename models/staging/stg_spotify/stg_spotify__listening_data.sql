da75bb6146a90dc3190a83fbbcf91d395bbb3b47
Damien Azzopardi <damien@datadidakt.com>
2025-07-29 15:31:42 +0000

    Enable SQLFluff and lint Spotify models

--- models/staging/stg_spotify/stg_spotify__listening_data.sql
+++ models/staging/stg_spotify/stg_spotify__listening_data.sql
@@ -1,14 +1,14 @@
 with
-    cleaned_listening_data as (
+cleaned_listening_data as (
 
-        select
-            song_id,
-            cast(listen_date as date) as listen_date,
-            coalesce(minutes_listened, 0) as minutes_listened
+    select
+        song_id,
+        cast(listen_date as date) as listen_date,
+        coalesce(minutes_listened, 0) as minutes_listened
 
-        from {{ source("spotify", "listening_data") }}
+    from {{ source("spotify", "listening_data") }}
 
-    )
+)
 
 select *
 from cleaned_listening_data
--- models/staging/stg_spotify/stg_spotify__songs.sql
+++ models/staging/stg_spotify/stg_spotify__songs.sql
@@ -1,17 +1,17 @@
 with
-    cleaned_songs as (
+cleaned_songs as (
 
-        select
-            song_id,
-            upper(title) as title,
-            upper(artist) as artist,
-            album,
-            release_year,
-            coalesce(genre, 'Unknown') as genre
+    select
+        song_id,
+        album,
+        release_year,
+        upper(title) as title,
+        upper(artist) as artist,
+        coalesce(genre, 'Unknown') as genre
 
-        from {{ source("spotify", "songs") }}
+    from {{ source("spotify", "songs") }}
 
-    )
+)
 
 select *
 from cleaned_songs
